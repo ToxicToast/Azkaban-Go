2b1b4de8ee25860682337108abb6165febd65dfd
Thomas Kortyka <t.kortyka@gmx.de>
2025-08-06 14:49:10 +0200

    chore: update magefiles

--- magefiles/proto.go
+++ magefiles/proto.go
@@ -14,6 +14,8 @@ var (
 	protoDir   = "libs/proto"
 	outputDir  = "libs/shared/proto"
 	protoFiles = []string{
+		"shared/common.proto",
+		//
 		"foodfolio/category.proto",
 		"foodfolio/company.proto",
 		"foodfolio/item.proto",
@@ -22,8 +24,25 @@ var (
 		"foodfolio/type.proto",
 		"foodfolio/warehouse.proto",
 		//
+		"warcraft/common.proto",
+		//
+		"warcraft/types/character-type.proto",
+		"warcraft/types/character-requests.proto",
+		"warcraft/types/character-responses.proto",
+		"warcraft/types/guild-type.proto",
+		"warcraft/types/guild-responses.proto",
+		//
 		"warcraft/character.proto",
 		"warcraft/guild.proto",
+		//
+		"warcraft/cronjobs/cronjob-search.proto",
+		"warcraft/cronjobs/cronjob-guild.proto",
+		"warcraft/cronjobs/cronjob-guild-roster.proto",
+		"warcraft/cronjobs/cronjob-character-profile.proto",
+		"warcraft/cronjobs/cronjob-media.proto",
+		"warcraft/cronjobs/cronjob-mythic.proto",
+		//
+		"warcraft/cronjob.proto",
 		//
 		"twitch/viewer.proto",
 	}
